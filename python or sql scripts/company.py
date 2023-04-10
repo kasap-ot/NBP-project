7a47cb5b9c5bd2a7b5cacd617594be36ea2f5e4e
kasap-ot <63372251+kasap-ot@users.noreply.github.com>
2023-04-10 18:37:09 +0200

    Refactor python script

--- python or sql scripts/company.py
+++ python or sql scripts/company.py
@@ -5,18 +5,21 @@ import csv
 
 #user-defined functions
 #Дефинираме функции по потреба.
+
 def random_alphabetic_sequence_with_random_length(min_length, max_length):
     N = random.randint(min_length, max_length)
     sequence = ""
     for i in range(0,N):
         sequence= sequence + random.choice(string.ascii_letters)
     return sequence
+
 def random_alphanumeric_sequence_with_random_length(min_length, max_length):
     N = random.randint(min_length, max_length)
     sequence = ""
     for i in range(0,N):
         sequence= sequence + random.choice(string.ascii_letters + string.digits)
     return sequence
+
 def random_digits_sequence(length):
     sequence = ""
     for i in range(length):
@@ -26,18 +29,23 @@ def random_digits_sequence(length):
 
 if __name__ == '__main__':
     random.seed()
+    
     # country_table_length  чува број на редови внесени во табелата COUNTRY.
     # Овој број ни е потребен за да знаеме кој е максималното ID во табелта COUNTRY.
     # И потоа бираме случајна вредост од 1 до country_table_length, и избраната вредност ја користиме како надворешен клуч
     # Значи, колку што имаме надворешни клучеви за една табела, толку променливи треба да имаме од ваков тип,за да се
     # осигураме дека надворешниот клуч секогаш ќе е во рангот на 1 до најголемата-вредност-на индексот т.е вредноста на ова променлива.
+    
     country_table_length = pd.read_csv('../resources/csv_files/all_countries_only_names.csv', header=None).shape[0]
+    
     # Патеката вие си ја избирате, само внимавајте да постои таква хиерархија од фолдери xD
 
     # Бројот на редови зависи од самата табела. Нека биде 1000 на пример па потоа ќе видиме како понатака.
     total_rows = 1000
+    
     with open('../resources/csv_files/company.csv', 'w', newline='') as file:
         writer = csv.writer(file)
+        
         for i in range(0,total_rows):
             # За секој атрибут се генерира вредност.Доколку id-то на соодветната табела не се генерира автоматски тогаш мора да го генерираме.
             # Сите id што имаат тип "serial" во ddl-от се генерираат автоматски.
