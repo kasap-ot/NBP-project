f7bccd8956229337712cbd119edde9303f65c920
kasap-ot <63372251+kasap-ot@users.noreply.github.com>
2023-04-26 01:18:02 +0200

    Update offers script. Generate 10k offers

--- python_or_sql_scripts/offer.py
+++ python_or_sql_scripts/offer.py
@@ -7,8 +7,26 @@ start_time = time.time()
 
 random.seed()
 
+# ____________________________________________________________________
+
+# First we need to read the ids of the members 
+# so we can later insert them as foreign keys
+
+member_ids = list()
+members_file_path = 'csv_files/member.csv'
+
+with open(members_file_path, 'r') as members_file:
+    csv_reader = csv.reader(members_file)
+    for row in csv_reader:
+        for item in row:
+            member_ids.append(int(item))
+
+member_ids_length = len(member_ids)
+
+# ____________________________________________________________________
+
 # NOTE: We should define this. For 1000 rows it takes about 1 sec.
-total_rows = 10 
+total_rows = 100_000
 
 with open('csv_files/offer.csv', 'w', newline='') as file:
     writer = csv.writer(file)
@@ -19,11 +37,11 @@ with open('csv_files/offer.csv', 'w', newline='') as file:
         benefits = utils.random_word_sequence(15)
         salary = random.randint(500, 5000)
         field = utils.random_word_sequence(3)
-        start_date = utils.random_date(2023, 2024)
+        start_date = utils.random_date('2023-01-01', '2024-12-31')
         duration_in_weeks = random.randint(4, 52)
         
-        member_id = 1 # TODO: For now just a filler reference to the member-creator
-        company_id = 1 # TODO: For now just a filler reference
+        member_id = member_ids[random.randint(0, member_ids_length-1)]
+        company_id = random.randint(1, 1000) # There are 1000 rows in company.csv
 
         # NOTE: Uncomment when we decide to run this script
         writer.writerow([requirements,
