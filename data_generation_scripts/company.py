fd78f73fd8c2a77015d33471775044e2df97b036
Hristijan Dailovski <hristijan.dailovski@outlook.com>
2023-04-26 12:34:04 +0200

    update company.csv and company.py

--- data_generation_scripts/company.py
+++ data_generation_scripts/company.py
@@ -14,32 +14,29 @@ if __name__ == '__main__':
     country_table_length = pd.read_csv('../csv_files/country.csv', header=None).shape[0]
     # Патеката вие си ја избирате, само внимавајте да постои таква хиерархија од фолдери xD
 
-    # Бројот на редови зависи од самата табела. Нека биде 1000 на пример па потоа ќе видиме како понатака.
-    total_rows = 1000
+    companies_per_country= 50
     with open('../csv_files/company.csv', 'w', newline='') as file:
         writer = csv.writer(file)
-        for i in range(0,total_rows):
-            # За секој атрибут се генерира вредност.Доколку id-то на соодветната табела не се генерира автоматски тогаш мора да го генерираме.
-            # Сите id што имаат тип "serial" во ddl-от се генерираат автоматски.
-            # Останатите имаат тип "integer" и мора да се генерирарат рачно преку генерирање на случаен број во ранг од 1 до name_table_length.
+        for country_id in range(1,country_table_length+1):
+            for offset in range(companies_per_country):
+                # За секој атрибут се генерира вредност.Доколку id-то на соодветната табела не се генерира автоматски тогаш мора да го генерираме.
+                # Сите id што имаат тип "serial" во ddl-от се генерираат автоматски.
+                # Останатите имаат тип "integer" и мора да се генерирарат рачно преку генерирање на случаен број во ранг од 1 до name_table_length.
 
-            #name
-            name = "company " + utils.random_alphabetic_sequence_with_random_length(10,30-len("company "))
+                #name
+                name = "company " + utils.random_syllable_sequence(random.randint(4,6))
 
-            #phone with pattern '[0-9]{9}'
-            phone_number = utils.random_phone_number()
-            #email with pattern '^[\w\-\.]+@([\w\-]+\.)+[\w\-]{2,4}$'
-            email_address = utils.random_email()
-            #address
-            address = utils.random_address()
-            #country_id
-            country_id = str(random.randint(1,country_table_length))
+                #phone with pattern '[0-9]{9}'
+                phone_number = utils.random_phone_number()
+                #email with pattern '^[\w\-\.]+@([\w\-]+\.)+[\w\-]{2,4}$'
+                email_address = utils.random_email()
+                #address
+                address = utils.random_address()
+                #number_of_employees
+                number_of_employees = str(random.randint(100,1000))
 
-            #number_of_employees
-            number_of_employees = str(random.randint(100,1000))
-
-            # На крај го запишуваме редот во фајлот.
-            writer.writerow([name,phone_number,email_address,address,country_id,number_of_employees])
+                # На крај го запишуваме редот во фајлот.
+                writer.writerow([name,phone_number,email_address,address,str(country_id),number_of_employees])
 
 
 
