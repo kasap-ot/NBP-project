3a256ed48e3a24e8ed33bf9c147dad83e4f3eb7e
kasap-ot <63372251+kasap-ot@users.noreply.github.com>
2023-05-03 17:51:33 +0200

    Add 2 views

--- views.sql
+++ views.sql
@@ -29,3 +29,27 @@ create view COMPLETED_INTERNSHIP as
 select id as internship_id, grade_work, grade_accommodation,grade_student,((grade_student+grade_accommodation+grade_work)/3) as average_grade, comment_student, comment_company,duration_in_weeks,salary,bonus_pay,student_id,offer_id,acceptance_status,date_of_app_submission
 from INTERNSHIP join COMPLETED_OFFER on (offer_id = applies_for_offer_id and student_id = applies_for_student_id)
 order by average_grade desc ;
+
+
+----------------- Kasap's views -----------------
+
+create view student_completed_offers as
+select end_user.name, end_user.surname, count(applies_for.acceptance_status)
+from end_user 
+inner join student on end_user.id = student.id
+left join applies_for on student.id = applies_for.student_id
+where applies_for.acceptance_status = 'COMPLETED'
+group by end_user.name;
+
+
+create view full_user_info as
+select u.name, u.surname, u.date_of_birth, u.address, u.phone_number, u.email_address, country.name
+from end_user as u
+inner join country on u.country_id = country.id;
+
+
+-- Student with his qualifications - will have to many columns?
+
+-- Students ordered by faculty type - we don't yet have the faculty type column...
+
+-- Students by average grade on internship - challenging
