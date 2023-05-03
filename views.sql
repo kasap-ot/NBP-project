85236a2103dff4cd0df0ef2e2dd4d00c49676f93
tashevbrane <branetasev10@gmail.com>
2023-05-03 18:51:14 +0200

    add Brane's and Evica's views

--- views.sql
+++ views.sql
@@ -53,3 +53,84 @@ inner join country on u.country_id = country.id;
 -- Students ordered by faculty type - we don't yet have the faculty type column...
 
 -- Students by average grade on internship - challenging
+
+
+----------------- Brane's views -----------------
+--offers by company
+create view offers_by_company as (select c.name as Company_Name,
+                                  o.field as Field,
+                                  o.requirements as Requirements,
+                                  o.responsibilities as Responsibilities,
+                                  o.benefits as Benefits,
+                                  o.salary as Salary,
+                                  o.start_date as Start,
+                                  o.duration_in_weeks as Duration
+                           from offer o
+                                    join company c on o.company_id = c.id);
+select * from offers_by_company;
+
+-- - events by type
+create view event_type as
+select 'training' as event_type, e.name as event_name
+from event e
+join training t on e.id = t.id
+union
+select 'competition' as event_type, e.name as event_name
+from event e
+join competition c on e.committee_id = c.id;
+
+select * from event_type;
+
+-- companies with average grade on its internships
+select c.name, avg(grade_work)
+from company c
+    join offer o on c.id = o.company_id
+    join internship i on o.id = i.applies_for_offer_id
+group by 1;
+
+
+-- - companies with its number of offers
+create view companies_with_number_of_offers as
+(select c.name, count(o.id) as number_offers
+from company c
+         join offer o on c.id = o.company_id
+group by c.id, c.name);
+
+select * from companies_with_number_of_offers;
+
+
+
+----------------- Evica's views -----------------
+-- - committees with number of offers created
+create view offers_from_comittees as (
+select c.id as Comittee_Id, count(o.id) as Number_Offers
+from offer o join member_of mo on o.member_id = mo.member_id join committee c on mo.committee_id = c.id
+group by c.id
+);
+select * from Offers_From_Comittees;
+
+-- - committees with average grade of all of its offers
+--ne mora ne treba :D
+--from offer o join member_of mo on o.member_id = mo.member_id join committee c on mo.committee_id = c.id
+
+-- - committess with its country
+create view committees_countries as
+(
+select committee.id as committee_id, country.name as country
+from committee
+         join country on committee.country_id = country.id
+    );
+select * from committees_countries;
+
+
+-- - members by organization
+create view members_organizations as
+(
+select e.name, e.surname, o.name
+from end_user e
+         join member_of mo on e.id = mo.member_id
+         join committee c on mo.committee_id = c.id
+         join organization o on c.org_id = o.id
+    );
+select * from members_organizations;
+
