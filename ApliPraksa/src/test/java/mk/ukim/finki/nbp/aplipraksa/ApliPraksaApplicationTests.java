b286dbf23e900f17fac236f820f5280fb058bc18
tashevbrane <branetasev10@gmail.com>
2023-06-16 19:39:29 +0200

    add IntelliJ folder

---
+++ ApliPraksa/src/test/java/mk/ukim/finki/nbp/aplipraksa/ApliPraksaApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.nbp.aplipraksa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApliPraksaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
