100f1cba4ef468fa8596391a9e3c9344f3c24642
kasap-ot <63372251+kasap-ot@users.noreply.github.com>
2023-06-21 15:25:51 +0200

    Add form for student and member registration. Add form for company creation

--- ApliPraksa/src/main/java/mk/ukim/finki/nbp/aplipraksa/controller/CompanyController.java
+++ ApliPraksa/src/main/java/mk/ukim/finki/nbp/aplipraksa/controller/CompanyController.java
@@ -1,5 +1,16 @@
 package mk.ukim.finki.nbp.aplipraksa.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
 public class CompanyController {
-    
+    @GetMapping("/company")
+    public String getAboutUsPage(Model model){
+        model.addAttribute("bodyContent", "add-company");
+        return "master-template";
+    }
 }
--- ApliPraksa/src/main/java/mk/ukim/finki/nbp/aplipraksa/controller/RegisterController.java
+++ ApliPraksa/src/main/java/mk/ukim/finki/nbp/aplipraksa/controller/RegisterController.java
@@ -8,9 +8,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/")
 public class RegisterController {
-    @GetMapping("/register")
+    @GetMapping("/register-as-student")
     public String getHomePage(Model model){
-        model.addAttribute("bodyContent", "register");
+        model.addAttribute("bodyContent", "register-as-student");
+        return "master-template";
+    }
+
+    @GetMapping("/register-as-member")
+    public String getRegisterAsMemberPage(Model model) {
+        model.addAttribute("bodyContent", "register-as-member");
         return "master-template";
     }
 }
