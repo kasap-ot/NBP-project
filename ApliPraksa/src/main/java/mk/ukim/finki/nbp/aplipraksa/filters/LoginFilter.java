aca5f939b5cc3f0c1ae95d5928177f724ff43fb4
Hristijan Dailovski <hristijan.dailovski@outlook.com>
2023-06-27 10:35:06 +0200

    refactor sessionless behaviour

--- ApliPraksa/src/main/java/mk/ukim/finki/nbp/aplipraksa/filters/LoginFilter.java
+++ ApliPraksa/src/main/java/mk/ukim/finki/nbp/aplipraksa/filters/LoginFilter.java
@@ -9,9 +9,21 @@ import jakarta.servlet.http.HttpServletResponse;
 import mk.ukim.finki.nbp.aplipraksa.model.UserCredentials;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.stream.Stream;
 
 @WebFilter()
 public class LoginFilter implements Filter {
+    static private String[] sessionlessPaths = {
+            "/register-as-student",
+            "/register-as-member",
+            "/login",
+            "/home",
+            "/about-us"
+    };
+    static private boolean isPathSessionless(String path){
+        return Arrays.asList(sessionlessPaths).contains(path);
+    }
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
 
@@ -32,8 +44,8 @@ public class LoginFilter implements Filter {
             //continue
             filterChain.doFilter(servletRequest,servletResponse);
 
-        }else if(!path.equals("/login") && userCredentials==null){
-                response.sendRedirect("/login");
+        }else if(!(isPathSessionless(path))&& userCredentials==null){
+                response.sendRedirect("/home");
             }else{
                 filterChain.doFilter(servletRequest,servletResponse);
             }
